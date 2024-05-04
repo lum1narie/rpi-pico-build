73c14adfbae3c7e044a0c5a8dcf5664ba80f6c67
lum1narie <sn.lum.fallenrose.aol@gmail.com>
2024-05-05 06:21:39 +0900

    feat: :sparkles: now entrypoint can get arguments to set CMake build target

--- build_pico.sh
+++ build_pico.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
-for TARGET in $(find /target -mindepth 1 -maxdepth 1 -type d); do
-    cd $TARGET
+if [ $# -gt 0 ]; then
+    CMAKE_TARGET="-t $1"
+else
+    CMAKE_TARGET=
+fi
+
+for PROJECT in $(find /target -mindepth 1 -maxdepth 1 -type d); do
+    cd $PROJECT
     cmake -H. -Bbuild -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -GNinja
-    cmake --build build -j$(echo "$(nproc) + 1" | bc)
+    cmake --build build ${CMAKE_TARGET} -j$(($(nproc)+1))
 done
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /home/$LOCAL_USER
-exec env CMAKE_COLOR_DIAGNOSTICS=always /usr/sbin/gosu $LOCAL_USER /bin/bash -c '$HOME/build_pico.sh'
+exec env CMAKE_COLOR_DIAGNOSTICS=always /usr/sbin/gosu $LOCAL_USER /bin/bash -c '$HOME/build_pico.sh '"$@"
